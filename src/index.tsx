316bcbcff697ea2d3660ba590b2038bfc6862d11
Vladyslav Lytvynenko <vladyslav.lytvynenko22@gmail.com>
2021-02-06 02:57:52 +0200

    Adding Middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
 
 import addCarRducer from './store/reducers/add-car';
 import appRducer from './store/reducers/app';
@@ -13,7 +13,17 @@ const rootReducer = combineReducers({
   addCarRducer: addCarRducer,
   appRducer: appRducer,
 });
-const store = createStore(rootReducer);
+const logger = (store: any) => {
+  return (nextArg: any) => {
+    return (action: any) => {
+      console.log('[Middleware] Dispatching', action);
+      const result = nextArg(action);
+      console.log('[Middleware] next state', store.getState());
+      return result;
+    };
+  };
+};
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
